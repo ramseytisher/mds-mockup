5773abadb2071611d568a5ee30a8de45d6f16b43
rt022748 <ramsey.tisher@cerner.com>
2020-01-06 13:52:14 -0500

    field history

--- src/components/details/field-history.js
+++ src/components/details/field-history.js
@@ -1,21 +1,29 @@
 import React from "react"
 
-import { Typography, Card } from "antd"
+import { Typography, Card, Timeline, Descriptions } from "antd"
 const { Title, Text } = Typography
 
 export default () => (
-  <Text>
-    Lorem ipsum dolor amet yuccie try-hard magna adipisicing yr banh mi.
-    Fingerstache scenester brunch, tattooed glossier pariatur celiac thundercats
-    polaroid. Man bun pinterest post-ironic 90's, chia veniam green juice
-    flannel tote bag. Semiotics tofu leggings, fugiat dolor deserunt aute
-    mixtape air plant flexitarian. Gluten-free consectetur street art minim
-    mollit next level chambray hammock. Live-edge mlkshk XOXO cred pour-over
-    excepteur freegan veniam officia taiyaki truffaut anim squid af. Lyft nisi
-    schlitz, hell of cornhole squid aliquip. Yr dolore fixie, squid sartorial
-    deep v neutra hell of shabby chic lo-fi paleo celiac messenger bag palo
-    santo. Quinoa pug dolore excepteur offal cray normcore af mixtape succulents
-    occaecat keytar tumblr mustache direct trade. Consequat cillum mumblecore
-    pok pok poutine, flannel voluptate.
-  </Text>
+  <Timeline bordered>
+    <Timeline.Item>
+      <Descriptions title="1/1/2020">
+        <Descriptions.Item label="Response Change">
+          <Text code>0. No</Text>
+          {" > "}
+          <Text code>1. Yes</Text>
+        </Descriptions.Item>
+        <Descriptions.Item label="User">John Smith</Descriptions.Item>
+      </Descriptions>
+    </Timeline.Item>
+    <Timeline.Item>
+      <Descriptions title="12/1/2019">
+        <Descriptions.Item label="Response Change">
+          <Text code>(Empty)</Text>
+          {" > "}
+          <Text code>0. No</Text>
+        </Descriptions.Item>
+        <Descriptions.Item label="User">Jane Doe</Descriptions.Item>
+      </Descriptions>
+    </Timeline.Item>
+  </Timeline>
 )
