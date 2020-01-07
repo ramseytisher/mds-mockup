5c12c21db581e80cd25d777b81c3fda370619601
rt022748 <ramsey.tisher@cerner.com>
2020-01-07 16:16:42 -0500

    added QM stuff

--- src/components/details/quality-details.js
+++ src/components/details/quality-details.js
@@ -1,21 +1,126 @@
 import React from "react"
 
-import { Typography, Card } from "antd"
+import { Typography, Card, Tag, Collapse, Tree } from "antd"
 const { Title, Text } = Typography
+const { Panel } = Collapse
+const { TreeNode } = Tree
 
 export default () => (
-  <Text>
-    Lorem ipsum dolor amet yuccie try-hard magna adipisicing yr banh mi.
-    Fingerstache scenester brunch, tattooed glossier pariatur celiac thundercats
-    polaroid. Man bun pinterest post-ironic 90's, chia veniam green juice
-    flannel tote bag. Semiotics tofu leggings, fugiat dolor deserunt aute
-    mixtape air plant flexitarian. Gluten-free consectetur street art minim
-    mollit next level chambray hammock. Live-edge mlkshk XOXO cred pour-over
-    excepteur freegan veniam officia taiyaki truffaut anim squid af. Lyft nisi
-    schlitz, hell of cornhole squid aliquip. Yr dolore fixie, squid sartorial
-    deep v neutra hell of shabby chic lo-fi paleo celiac messenger bag palo
-    santo. Quinoa pug dolore excepteur offal cray normcore af mixtape succulents
-    occaecat keytar tumblr mustache direct trade. Consequat cillum mumblecore
-    pok pok poutine, flannel voluptate.
-  </Text>
+  <>
+    <Collapse>
+      <Panel header="Independent">
+        <Tree defaultExpandAll>
+          <TreeNode title="21 - Residents who self-report moderate to severe pain">
+            <TreeNode
+              title={
+                <>
+                  <Tag>Casper</Tag>
+                  <Tag>Five Star</Tag>
+                  <Tag>Nursing Home Compare</Tag>
+                </>
+              }
+            ></TreeNode>
+          </TreeNode>
+        </Tree>
+      </Panel>
+      <Panel header="Modified independence">
+        <Tree defaultExpandAll>
+          <TreeNode title="21 - Residents who self-report moderate to severe pain">
+            <TreeNode
+              title={
+                <>
+                  <Tag>Casper</Tag>
+                  <Tag>Five Star</Tag>
+                  <Tag>Nursing Home Compare</Tag>
+                </>
+              }
+            ></TreeNode>
+          </TreeNode>
+        </Tree>
+      </Panel>
+      <Panel header="Moderately impaired">N/A</Panel>
+      <Panel header="Severely impaired">
+        <Tree defaultExpandAll>
+          <TreeNode title="6 - Residents who made improvements in function">
+            <TreeNode
+              title={
+                <>
+                  <Tag>Casper</Tag>
+                  <Tag>Five Star</Tag>
+                  <Tag>Nursing Home Compare</Tag>
+                </>
+              }
+            ></TreeNode>
+          </TreeNode>
+        </Tree>
+        <Tree defaultExpandAll>
+          <TreeNode title="37 - Residents whose ability to move independently worsened">
+            <TreeNode
+              title={
+                <>
+                  <Tag>Casper</Tag>
+                  <Tag>Five Star</Tag>
+                  <Tag>Nursing Home Compare</Tag>
+                </>
+              }
+            />
+          </TreeNode>
+        </Tree>
+        <Tree defaultExpandAll>
+          <TreeNode title="MDS COVARIATES for CLAIMS-BASED QUALITY MEASURES">
+            <TreeNode title="1 - Short-stay residents who were re-hospitalized after a nursing home admission">
+              <TreeNode
+                title={
+                  <>
+                    <Tag>Five Star</Tag>
+                    <Tag>Nursing Home Compare</Tag>
+                  </>
+                }
+              />
+            </TreeNode>
+            <TreeNode title="2 - Number of hospitalizations per 1,000 long-stay resident days">
+              <TreeNode
+                title={
+                  <>
+                    <Tag>Five Star</Tag>
+                    <Tag>Nursing Home Compare</Tag>
+                  </>
+                }
+              />
+            </TreeNode>
+            <TreeNode title="3 - Short-stay residents who have had an outpatient emergency department visit">
+              <TreeNode
+                title={
+                  <>
+                    <Tag>Five Star</Tag>
+                    <Tag>Nursing Home Compare</Tag>
+                  </>
+                }
+              />
+            </TreeNode>
+            <TreeNode title="4 - Number of outpatient emergency department visits per 1,000 long-stay resident days">
+              <TreeNode
+                title={
+                  <>
+                    <Tag>Five Star</Tag>
+                    <Tag>Nursing Home Compare</Tag>
+                  </>
+                }
+              />
+            </TreeNode>
+            <TreeNode title="5 - Discharge to community">
+              <TreeNode
+                title={
+                  <>
+                    <Tag>Five Star</Tag>
+                    <Tag>Nursing Home Compare</Tag>
+                  </>
+                }
+              />
+            </TreeNode>
+          </TreeNode>
+        </Tree>
+      </Panel>
+    </Collapse>
+  </>
 )
