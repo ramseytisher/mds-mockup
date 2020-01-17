ab64f4996d56bb181e82cb506929620d5860e65c
rt022748 <ramsey.tisher@cerner.com>
2020-01-17 08:21:14 -0500

    affixed header

--- src/components/header.js
+++ src/components/header.js
@@ -3,47 +3,65 @@ import React from "react"
 
 import Login from "./login"
 
-import { Row, Col, Button, Popconfirm, Typography, message, Icon } from "antd"
+import {
+  Affix,
+  Row,
+  Col,
+  Button,
+  Popconfirm,
+  Typography,
+  message,
+  Icon,
+} from "antd"
 const { Title, Text } = Typography
 
 const Header = () => (
-  <header
-    style={{
-      background: `#fff`,
-      margin: `0 auto`,
-      padding: `.5rem 1.0875rem`,
-    }}
-  >
-    <Row type="flex" align="middle" justify="space-between">
-      <Col>
-        <Title>
-          <Link
-            to="/"
-            style={{
-              textDecoration: `none`,
-              color: "#5C6770",
-            }}
-          >
-            <Icon type="highlight" theme="twoTone" />{" "}
-            MDS Mockup
+  <Affix>
+    <header
+      style={{
+        background: `#fff`,
+        margin: `0 auto`,
+        paddingLeft: `1.0875rem`,
+        paddingRight: `1.0875rem`
+      }}
+    >
+      <Row type="flex" align="middle" justify="space-between">
+        <Col>
+          <Title>
+            <Link
+              to="/"
+              style={{
+                textDecoration: `none`,
+                color: "#5C6770",
+              }}
+            >
+              <Icon type="highlight" theme="twoTone" /> MDS Mockup
+            </Link>
+          </Title>
+        </Col>
+        <Col>
+          <Link to="/assessment">
+            <Button type="link" size="large">
+              Assessment
+            </Button>
           </Link>
-        </Title>
-      </Col>
-      <Col>
-        <Link to="/assessment">
-          <Button type="link" size="large">
-            Assessment
-          </Button>
-        </Link>
-        <Link to="/schedule">
-          <Button type="link" size="large" disabled>
-            Schedule
-          </Button>
-        </Link>
-        <Login />
-      </Col>
-    </Row>
-  </header>
+          <Link to="/schedule">
+            <Button type="link" size="large" disabled>
+              Schedule
+            </Button>
+          </Link>
+          <Login />
+        </Col>
+      </Row>
+      <hr
+        style={{
+          border: 0,
+          height: 0,
+          boxShadow: "0 0 12px 1px black",
+        }}
+      />
+    </header>
+  </Affix>
 )
 
 export default Header
