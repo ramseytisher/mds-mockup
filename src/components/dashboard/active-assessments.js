99a1139c64ab6e593456f34a037e4047e74e01e8
rt022748 <ramsey.tisher@cerner.com>
2020-03-04 12:32:20 -0500

    cleaned up

--- src/components/dashboard/active-assessments.js
+++ src/components/dashboard/active-assessments.js
@@ -10,22 +10,3 @@ const data = [
 export default () => (
     <pre>{JSON.stringify(data, null, 2)}</pre>
 )
-
-// This was an attempt to use a chart library that failed, worked locally but not on production build
-// import { Chart, Geom, Axis, Tooltip, Legend, Coord } from "bizcharts"
-
-
-
-// const cols = {
-//   assessments: { alias: "Assessments" },
-//   type: { alias: "MDS Type" },
-// }
-
-// export default () => (
-//   <Chart width={300} height={400} data={data} scale={cols}>
-//     <Axis name="assessments" title />
-//     <Coord transpose />
-//     <Tooltip />
-//     <Geom type="interval" position="type*assessments" color="type" />
-//   </Chart>
-// )
--- src/components/dashboard/search-query.js
+++ src/components/dashboard/search-query.js
@@ -1,8 +1,4 @@
 import React from "react"
-import styled from "styled-components"
-
-import { Chart, Geom, Axis, Tooltip, Legend, Coord } from "bizcharts"
-import DataSet from "@antv/data-set"
 
 const data = [
   { label: "Query 1", assessments: 12, residents: 11 },
@@ -15,36 +11,3 @@ const data = [
 export default () => (
     <pre>{JSON.stringify(data, null, 2)}</pre>
 )
-
-// This was an attempt to use a chart library that failed, worked locally but not on production build
-
-// const cols = {
-//   assessments: { alias: "Assessments" },
-//   residents: { alias: "Residents" },
-//   type: { alias: "MDS Type" },
-// }
-
-// const ds = new DataSet()
-// const dv = ds.createView().source(data)
-
-// dv.transform({
-//   type: "fold",
-//   fields: ["assessments", "residents"],
-//   key: "type",
-//   value: "value",
-// })
-
-// export default () => (
-//   <Chart width={500} height={400} data={dv} scale={cols}>
-//     <Coord transpose />
-//     <Axis name="value" position={"right"} />
-//     <Tooltip />
-//     <Geom
-//       type="interval"
-//       position="label*value"
-//       color="type"
-//       adjust={[{ type: "dodge" }]}
-//     />
-//     <Legend />
-//   </Chart>
-// )
